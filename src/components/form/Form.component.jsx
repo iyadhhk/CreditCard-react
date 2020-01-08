87ee22c408827f099aad58ea5c030dda8fa5e252
iyadhhk <iyadhbox@gmail.com>
2020-01-08 19:21:30 +0100

    use regex in conditions

--- src/components/form/Form.component.jsx
+++ src/components/form/Form.component.jsx
@@ -11,20 +11,19 @@ class Form extends React.Component {
       valid: ""
     };
   }
+
+  //  Card Number validation function using regex
+
   changeCardNum = e => {
-    const numReg = new RegExp(/^[0-9]*$/);
+    const numReg = new RegExp(/^[0-9]{0,16}$/);
     let cardNum = e.target.value;
     let prevState = this.state.cardNumber;
-    if (cardNum.length <= 16) {
-      if (isNaN(cardNum) || !numReg.test(cardNum)) {
-        e.target.value = prevState;
-      } else {
-        this.setState({ cardNumber: cardNum });
-      }
-    } else {
-      e.target.value = cardNum.substr(0, 16);
-    }
+    if (!numReg.test(cardNum)) { e.target.value = prevState; }
+    else { this.setState({ cardNumber: cardNum }); }
   };
+
+  // formatting card Number function : 5555 1111 6666 1111
+
   formatCardNumber = () => {
     let str = String(this.state.cardNumber).padEnd(16, "*");
     return str
@@ -33,55 +32,32 @@ class Form extends React.Component {
       .concat("  ", str.slice(8, 12))
       .concat("  ", str.slice(12));
   };
+
+  // Name's validation function using regex 
+
   changeName = e => {
-    const regName = new RegExp(/^[a-zA-Z ]+$/);
-    let nameInput = e.target.value.trim();
+    const regName = new RegExp(/(^[a-zA-Z]*$)|(^[a-zA-Z]+ $)|(^[a-zA-Z]+ [a-zA-Z]+$)/);
+    let nameInput = e.target.value;
+    let prevState = this.state.name;
     if (nameInput.length <= 20) {
-      if (regName.test(nameInput)) {
-        this.setState({ name: nameInput });
-      } else {
-        e.target.value = "";
-        this.setState({ name: "" });
-      }
-    } else {
-      e.target.value = nameInput.substr(0, 20);
-    }
+      if (regName.test(nameInput)) { this.setState({ name: nameInput }); }
+      else { e.target.value = prevState; }
+    } else { e.target.value = nameInput.substr(0, 20); }
   };
 
+  // Validation Date function using regex
+
   changeValid = e => {
-    let v = e.target.value;
+    const monthRegex = new RegExp(/^$|(^(0|1)$)|(^0[1-9]\/?[1-9]?[1-9]?$)|(^1[0-2]\/?[1-9]?[1-9]?$)/);
+    let date = e.target.value;
     let prevState = this.state.valid;
-    if (v === " ") {
-      e.target.value = "";
-      this.setState({ valid: "" });
-    } else {
-      if (v.length <= 5) {
-        if (v.length === 1) {
-          if (Number(v) === 0 || Number(v) === 1) {
-            this.setState({ valid: v });
-          } else {
-            e.target.value = "";
-            v = "";
-            this.setState({ valid: "" });
-          }
-        } else if (v.length === 2 && prevState.length === 1) {
-          if (Number(v) >= 1 && Number(v) <= 12) {
-            e.target.value = v + "/";
-            v += "/";
-            this.setState({ valid: v });
-          } else {
-            e.target.value = prevState;
-            v = prevState;
-            this.setState({ valid: v });
-          }
-        } else if (v.length >= 4 && isNaN(v.substr(3, 2))) {
-          e.target.value = prevState;
-        } else {
-          this.setState({ valid: v });
-        }
-      } else {
-        e.target.value = v.substr(0, 5);
+    if (monthRegex.test(date)) {
+      if (date.length === 2 && prevState.length === 1) {
+        e.target.value += "/";
       }
+      this.setState({ valid: date });
+    } else {
+      e.target.value = prevState;
     }
   };
   render() {
